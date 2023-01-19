950a25d91a34d3e878a197e3b7f6dde074299fe0
Lucas Sena <luk_senna@hotmail.com>
2023-01-19 17:09:45 -0300

    1

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -1,11 +1,45 @@
-import logo from "./logo.svg";
+//css
 import "./App.css";
+
+//react
+import { useCallback, useEffect, useState } from "react";
+
+//data
+import { wordsList } from ".data/words";
 import StartScreen from "./components/startScreen";
+import Game from "./components/game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  // início do jogo
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  // processos de input de letras
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  //reinicia o jogo
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
